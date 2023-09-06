c430ea940446e9f21860b4e257ee88bb331c1282
Ivan Boskovic <52816165+Av3nGeR0809@users.noreply.github.com>
2023-09-06 13:46:36 +0100

    Delete Chrome extension for SDs directory

--- Chrome extension for SDs/background.js
+++
@@ -1,5 +0,0 @@
-chrome.webNavigation.onCompleted.addListener(function(details) {
-  if (details.url && details.url.startsWith('https://drive.google.com/drive/my-drive')) {
-    chrome.tabs.update(details.tabId, {url: 'https://drive.google.com/drive/shared-drives'});
-  }
-});
